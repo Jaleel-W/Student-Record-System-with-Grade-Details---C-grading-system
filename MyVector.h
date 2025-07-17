efe1314116a9ca5e041faa94e25727b6945059ef
Jaleel-W <161672926+Jaleel-W@users.noreply.github.com>
2025-07-17 11:04:31 -0400

    MyVector.h

--- MyVector.h
+++ MyVector.h
@@ -1,47 +1,37 @@
 //Author Jaleel Williamson
-//
-//
-#include <vector>
-
 #ifndef MYVECTOR_H
 #define MYVECTOR_H
 
-// MyVector Class
 template <typename T>
 class MyVector {
 private:
-    T* data;
-    int size;
-    int capacity;
+    T* data = nullptr;
+    int size = 0;
+    int capacity = 0;
+
+    void resize() {
+        capacity = capacity ? capacity * 2 : 1;
+        T* newData = new T[capacity];
+        for (int i = 0; i < size; ++i) {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+    }
 
 public:
-    MyVector() : data(nullptr), size(0), capacity(0) {}
     ~MyVector() { delete[] data; }
 
     void push_back(const T& value) {
-        if (size == capacity) {
-            capacity = (capacity == 0) ? 1 : capacity * 2;
-            T* newData = new T[capacity];
-            for (int i = 0; i < size; ++i) {
-                newData[i] = data[i];
-            }
-            delete[] data;
-            data = newData;
-        }
+        if (size == capacity) resize();
         data[size++] = value;
     }
 
-    T retrieveInfoAtIndex(int index) const {
-        if (index >= 0 && index < size) {
-            return data[index];
-        }
-    
-    // Default return statement 
-    return T();
-}
-    int getSize() const {
-        return size;
+    const T& operator[](int index) const { 
+        return data[index]; 
     }
+
+    int getSize() const { return size; }
 };
 
-#endif //MYVECTOR_H
+#endif
