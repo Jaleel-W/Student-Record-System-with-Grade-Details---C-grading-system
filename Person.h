543345258da3f1ea49663e5d77c9182efed7d248
Jaleel-W <161672926+Jaleel-W@users.noreply.github.com>
2025-07-17 11:05:36 -0400

    Person.h

--- Person.h
+++ Person.h
@@ -1,55 +1,45 @@
 //Author Jaleel Williamson
-//
-//
-#include <iostream>
-#include "Pair.h"
-#include "MyVector.h"
-
 #ifndef PERSON_H
 #define PERSON_H
 
+#include "MyVector.h"
+#include "Pair.h"
+#include <iostream>
 
-// Person Class (Abstract)
 class Person {
-private:
-    MyVector<Pair<std::string, std::string>> contactInfo;
-    std::string firstName;
+protected:
     std::string lastName;
+    std::string firstName;
     int id;
+    MyVector<Pair<std::string, std::string>> contactInfo;
 
 public:
-    Person(const std::string& first, const std::string& last, int _id)
-        : firstName(first), lastName(last), id(_id) {}
-
-    virtual ~Person() {}
-
+    Person(const std::string& last, const std::string& first, int _id)
+        : lastName(last), firstName(first), id(_id) {}
+    
+    virtual ~Person() = default;
+    
     virtual void displayInfo() const = 0;
     virtual std::string getRole() const = 0;
 
-    std::string getFullName() const {
-        return firstName + " " + lastName;
+    std::string getFullName() const { 
+        return lastName + ", " + firstName; 
     }
-
-    int getId() const {
-        return id;
+    
+    int getId() const { 
+        return id; 
     }
 
     void addContactInfo(const std::string& key, const std::string& value) {
-        contactInfo.push_back(Pair<std::string, std::string>(key, value));
-    }
-
-    void updateContactInfo(const MyVector<Pair<std::string, std::string>>& newInfo) {
-        // Update contact information as needed
-        // This is a simplified implementation
-        // You may want to implement logic to merge or replace existing information
+        contactInfo.push_back({key, value});
     }
 
     void displayContactInfo() const {
         for (int i = 0; i < contactInfo.getSize(); ++i) {
-            Pair<std::string, std::string> info = contactInfo.retrieveInfoAtIndex(i);
-            std::cout << info.first << ": " << info.second << std::endl;
+            const auto& info = contactInfo[i];
+            std::cout << "  " << info.first << ": " << info.second << "\n";
         }
     }
 };
 
-#endif //PERSON_H
+#endif
