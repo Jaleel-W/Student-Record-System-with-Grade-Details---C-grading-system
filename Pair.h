b8f885ce7feb17ba3a8aa8ec9b3d8ddfd424fe7d
Jaleel-W <161672926+Jaleel-W@users.noreply.github.com>
2025-07-17 11:05:16 -0400

    Pair.h

--- Pair.h
+++ Pair.h
@@ -1,18 +1,13 @@
 //Author Jaleel Williamson
-//
-//
 #ifndef PAIR_H
 #define PAIR_H
 
-// Pair Class
 template <typename T1, typename T2>
-class Pair {
-public:
+struct Pair {
     T1 first;
     T2 second;
-
+    Pair() = default;
     Pair(T1 f, T2 s) : first(f), second(s) {}
-    Pair() {}
 };
 
-#endif //PAIR_H
+#endif
